33fbef308dea45de7c86dcc6b834a8473b485602
skyfly33 <imfly7@naver.com>
2015-08-14 14:13:11 +0900

    add ngCordova push plugin && ngCordova doc sample test.

--- www/js/app.js
+++ www/js/app.js
@@ -7,7 +7,7 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova'])
 
-    .run(function ($ionicPlatform) {
+    .run(function ($ionicPlatform, $cordovaPush, $cordovaActionSheet) {
         $ionicPlatform.ready(function () {
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
@@ -20,6 +20,71 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
                 // org.apache.cordova.statusbar required
                 StatusBar.styleLightContent();
             }
+
+            var androidConfig = {
+                "senderID": "627833707210",
+            };
+
+            var options = {
+                title: 'What do you want with this image?',
+                buttonLabels: ['Share via Facebook', 'Share via Twitter'],
+                addCancelButtonWithLabel: 'Cancel',
+                androidEnableCancelButton: true,
+                winphoneEnableCancelButton: true,
+                addDestructiveButtonWithLabel: 'Delete it'
+            };
+
+            document.addEventListener("deviceready", function () {
+                $cordovaPush.register(androidConfig).then(function (result) {
+                    // Success
+                    console.log("Register Success Result : " + result);
+                    alert("Register Success Result : " + result);
+                    $cordovaActionSheet.show(options)
+                        .then(function (btnIndex) {
+                            var index = btnIndex;
+                        });
+                }, function (err) {
+                    // Error
+                    console.log("Register Error Result : " + err);
+                    alert("Register Error Result : " + result);
+                });
+
+                $rootScope.$on('$cordovaPush:notificationReceived', function (event, notification) {
+                    switch (notification.event) {
+                        case 'registered':
+                            if (notification.regid.length > 0) {
+                                alert('registration ID = ' + notification.regid);
+                            }
+                            break;
+
+                        case 'message':
+                            // this is the actual push notification. its format depends on the data model from the push server
+                            alert('message = ' + notification.message + ' msgCount = ' + notification.msgcnt);
+                            break;
+
+                        case 'error':
+                            alert('GCM error = ' + notification.msg);
+                            break;
+
+                        default:
+                            alert('An unknown GCM event has occurred');
+                            break;
+                    }
+                });
+
+
+                // WARNING: dangerous to unregister (results in loss of tokenID)
+                $cordovaPush.unregister(options).then(function (result) {
+                    // Success!
+                    console.log("Unregister Success Result : " + result);
+                    alert("Unregister Success Result : " + result);
+                }, function (err) {
+                    // Error
+                    console.log("Unregister Error Result : " + err);
+                    alert("Unregister Error Result : " + err);
+                });
+
+            }, false);
         });
     })
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,22 @@
 angular.module('starter.controllers', ['starter.controllers.messageController', 'starter.controllers.configController', 'starter.controllers.chatController'])
 
-    .controller('DashCtrl', function ($scope) {
+    .controller('DashCtrl', function ($scope, $cordovaActionSheet) {
+
+        var options = {
+            title: 'What do you want with this image?',
+            buttonLabels: ['Share via Facebook', 'Share via Twitter'],
+            addCancelButtonWithLabel: 'Cancel',
+            androidEnableCancelButton: true,
+            addDestructiveButtonWithLabel: 'Delete it'
+        };
+
+        document.addEventListener("deviceready", function () {
+
+            $cordovaActionSheet.show(options)
+                .then(function (btnIndex) {
+                    var index = btnIndex;
+                });
+        }, false);
     })
 
     .controller('ContactCtrl', function ($scope) {
