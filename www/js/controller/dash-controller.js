3fc1a28b3c3892fac6c84efc139543b9ba0c6bc3
skyfly33 <imfly7@naver.com>
2015-08-15 10:53:52 +0900

    delete duplicate logic.

--- www/js/controller/dash-controller.js
+++ www/js/controller/dash-controller.js
@@ -5,22 +5,5 @@
 angular.module('starter.controllers.dashController', [])
 
     .controller('DashCtrl', function ($scope, $cordovaActionSheet) {
-
-        var options = {
-            title: 'What do you want with this image?',
-            buttonLabels: ['Share via Facebook', 'Share via Twitter'],
-            addCancelButtonWithLabel: 'Cancel',
-            androidEnableCancelButton: true,
-            addDestructiveButtonWithLabel: 'Delete it'
-        };
-
-        document.addEventListener("deviceready", function () {
-
-            $cordovaActionSheet.show(options)
-                .then(function (btnIndex) {
-                    var index = btnIndex;
-                });
-        }, false);
-
         $scope.name = "skyfly33";
-    })
+    });
